03c917b0579d5f26661115de3acfdcf0f9500fa7
Bennet Gallein <bg@esyoil.com>
2024-01-29 14:50:00 +0100

    fix: pass registry auth

--- src/update-handler.ts
+++ src/update-handler.ts
@@ -2,7 +2,7 @@ import { $ } from "bun";
 
 export const handleUpdate = async (stack: string, composeFile: string) => {
   return (
-    await $`docker stack deploy -c ${composeFile} ${stack} --prune`.cwd(
+    await $`docker stack deploy -c ${composeFile} ${stack} --prune --with-registry-auth`.cwd(
       `/home/${process.env.HOME_PATH}/stacks/${stack}`
     )
   ).stderr.toString();
